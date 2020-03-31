b8dc3035585ee185c7dadb990f89649b9cd2f8c7
tarcisio bruni rangel <tarcisiobruni@hotmail.com>
2020-03-31 08:29:55 -0300

    Complete time series example

--- corey-shafer-tutorials/chart-time-series.py
+++ corey-shafer-tutorials/chart-time-series.py
@@ -1,31 +1,44 @@
 import pandas as pd
-from datetime import datetime
+from datetime import datetime,timedelta
 from matplotlib import pyplot as plt
+from matplotlib import dates as mpl_dates
+
 
 plt.style.use('seaborn')
 
-dates = [
-    datetime(2019,5,24),
-    datetime(2019,5,25),
-    datetime(2019,5,26),
-    datetime(2019,5,27),
-    datetime(2019,5,28),
-    datetime(2019,5,29),
-    datetime(2019,5,30)
-]
+# dates = [
+#     datetime(2019,5,24),
+#     datetime(2019,5,25),
+#     datetime(2019,5,26),
+#     datetime(2019,5,27),
+#     datetime(2019,5,28),
+#     datetime(2019,5,29),
+#     datetime(2019,5,30)
+# ]
 
-y = [0,1,3,4,6,5,7]
+# y = [0,1,3,4,6,5,7]
 
-plt.plot_date(dates,y,linestyle='solid')
+# date_format = mpl_dates.DateFormatter('%b, %d %Y')
 
-# data =  pd.read_csv('data-time-series.csv')
-# price_date = data['Date']
-# price_close = data['Close']
+# plt.gca().xaxis.set_major_formatter(date_format)
 
-# plt.title('Bitcoin Prices')
-# plt.xlabel('Date')
-# plt.ylabel('Closing Price')
+data =  pd.read_csv('data-time-series.csv')
 
+data['Date'] = pd.to_datetime(data['Date'])
+data.sort_values('Date', inplace=True)
+
+price_date = data['Date']
+price_close = data['Close']
+
+plt.plot_date(price_date,price_close,linestyle='solid')
+
+plt.gcf().autofmt_xdate()
+
+plt.title('Bitcoin Prices')
+plt.xlabel('Date')
+plt.ylabel('Closing Price')
+
+# adds a padding to graph
 plt.tight_layout()
 
 plt.show()
